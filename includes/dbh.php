3654860f87b11460873170664715f93a82eaab62
Michael <maethridge@gmail.com>
2018-03-19 05:53:46 -0400

    Added includes folder with DB PHP connection script

---
+++ includes/dbh.php
@@ -0,0 +1,9 @@
+<?php
+    //Variables for DB connection
+    $serverName = "localhost";
+    $userName = "root";
+    $password = "";
+    $dbName = "hma";
+
+    $conn = mysqli_connect($serverName, $userName, $password, $dbName);
+?>
